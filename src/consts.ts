b0ca9af99b083443c15007b34a84e4ee3abba1dc
Ruofan Wong <meimprankster@gmail.com>
2023-02-09 17:08:41 +0800

    fix index page, header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Leo Wang";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Leo Wang's personal website";
+export const SITE_DESCRIPTION = "I'm a Web / Game Developer & Designer.";
