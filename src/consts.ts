e47697e1f01638939a04a9cbb264788e133c5a38
Ruofan Wong <meimprankster@gmail.com>
2023-02-09 17:24:54 +0800

    add website keywords

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = "Leo Wang's personal website";
 export const SITE_DESCRIPTION = "I'm a Web / Game Developer & Designer.";
+export const SITE_KEYWORDS =
+  "Leo Wang, Web Developer, Game Developer, Game Designer, Graphic Design, TypeScript, JavaScript, Unity";
