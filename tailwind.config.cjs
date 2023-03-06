8b9c475bb7c2b3edbaebfebf16878e971a4045f1
Ruofan Wong <meimprankster@gmail.com>
2023-03-06 23:52:08 +0800

    Change BG color, add web dev workshop article

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        warm: "#FFFFFB",
-        "warm-dark": "#1c1c1c",
-      },
+      colors: {},
     },
   },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
